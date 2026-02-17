5ee7cf56d254a83800e17f72f1212df4b8c74580
jeevanrajesh17 <jeevanrajesh17@gmail.com>
2026-02-17 14:22:30 +0530

    Create Oopbanner2.java
    UC2 completed

---
+++ Oopbanner2.java
@@ -0,0 +1,17 @@
+package oop.banner;
+
+public class Oopbanner {
+
+	public static void main(String[] args) {
+		System.out.println(" *****   *****   *****    ***** ");
+		System.out.println("**   ** **   ** **   **  **   **");
+		System.out.println("**   ** **   ** **   **  **     ");
+		System.out.println("**   ** **   ** *****     ***** ");
+		System.out.println("**   ** **   ** **             **");
+		System.out.println("**   ** **   ** **       **   **");
+		System.out.println(" *****   *****  **        ***** ");
+
+
+	}
+
+}
