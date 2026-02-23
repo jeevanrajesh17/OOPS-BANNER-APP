110b977ca3a1ca7d0a81d072e1782c24d5cfa164
Jeevan <jeevanrajesh17@gmail.com>
2026-02-23 10:32:29 +0530

    UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPS Banner App
+ * UC1 - Print OOPS
+ * @author Jeevan
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
